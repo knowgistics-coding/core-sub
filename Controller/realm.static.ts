806f13f4a375f02076ceadefd0676ef04bfbb7be
knowgistics-coding <63992663+knowgistics-coding@users.noreply.github.com>
2022-08-30 14:42:07 +0700

    remove realms

--- Controller/realm.static.ts
+++ Controller/realm.static.ts
@@ -1,16 +1,1 @@
-import { User } from "firebase/auth";
-import * as Realm from "realm-web";
-import { realmApp } from "../../../controllers/realm";
-
-export type RealmUser = Realm.User;
-
-export class RealmStatic {
-  static async signIn(user: User): Promise<RealmUser> {
-    const token = await user.getIdToken();
-    const realmUser = realmApp.logIn(Realm.Credentials.jwt(token));
-    return realmUser;
-  }
-  static async signOut(user:RealmUser){
-    return await user.logOut()
-  }
-}
+export {}
