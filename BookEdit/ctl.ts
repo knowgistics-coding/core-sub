732d5b2cc796d986443199b03264e0d1560e8cfe
9arM <sarayuch.msu@gmail.com>
2022-09-30 12:50:34 +0700

    update book

--- BookEdit/ctl.ts
+++ BookEdit/ctl.ts
@@ -1,8 +1,11 @@
 import { BookContent, BookRawData } from "../Controller";
 import { genKey } from "draft-js";
 import update from "react-addons-update";
+import { PostDocument } from "./post.add";
+import { onSnapshot, query, Unsubscribe, where } from "firebase/firestore";
+import { ClientPostCtl } from "../../../controllers/client.post";
 
-export class BookEditCtl {
+export class BookEditCtl extends ClientPostCtl {
   static readonly add = {
     folder: (
       data: Partial<BookRawData>,
@@ -14,6 +17,38 @@ export class BookEditCtl {
             contents.concat({ key: genKey(), title, type: "folder" }),
         },
       });
+    },
+    post: (data: Partial<BookRawData>, title: string): Partial<BookRawData> => {
+      return update(data, {
+        contents: {
+          $apply: (contents: BookContent[] = []) =>
+            contents.concat({ key: genKey(), title, type: "post" }),
+        },
+      });
+    },
+  };
+  static readonly folder = {
+    moveOut: (
+      data: Partial<BookRawData>,
+      folderIndex: number,
+      itemIndex: number
+    ): Partial<BookRawData> => {
+      const item = Object.assign(
+        {},
+        data?.contents?.[folderIndex]?.items?.[itemIndex],
+        {
+          key: genKey(),
+        }
+      );
+      if (item) {
+        return update(data, {
+          contents: {
+            [folderIndex]: { items: { $splice: [[itemIndex, 1]] } },
+            $apply: (contents) => contents.concat(item),
+          },
+        });
+      }
+      return data;
     },
   };
   static readonly remove = {
@@ -31,6 +66,19 @@ export class BookEditCtl {
       return update(data, {
         contents: { [folderIndex]: { items: { $splice: [[itemIndex, 1]] } } },
       });
+    },
+  };
+  static post = {
+    many: (callback: (docs: PostDocument[]) => void): Unsubscribe => {
+      return onSnapshot(
+        query(this.collection("posts"), where("type", "==", "post")),
+        (snapshot) => {
+          const docs = snapshot.docs.map((doc) =>
+            this.parseDoc<PostDocument>(doc)
+          );
+          callback(docs);
+        }
+      );
     },
   };
 }
---
+++ BookEdit/post.add.tsx
@@ -0,0 +1,138 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useCore } from "../context";
+import { DataGrid } from "../DataGrid";
+import { KuiButton } from "../KuiButton";
+import { PageContentTypes } from "../PageEdit";
+import { StockDisplayProps } from "../StockDisplay";
+import { BookEditCtl } from "./ctl";
+import moment from "moment";
+import { KuiActionIcon } from "../KuiActionIcon";
+import { GridSelectionModel } from "@mui/x-data-grid";
+
+export type PostSchedule = {
+  start: string;
+  end: string;
+  timezone: string;
+};
+
+export interface PostDocument {
+  user: string;
+  title: string;
+  datecreate: Timestamp | Date | number;
+  datemodified: Timestamp | Date | number;
+  feature?: StockDisplayProps;
+  type: "post";
+  visibility?: string;
+  id: string;
+  desc?: string;
+  schedule?: PostSchedule;
+  contents?: PageContentTypes[];
+}
+
+export type PostAddType = {
+  open: boolean;
+  onClose: () => void;
+  onAddPost: (selection: PostDocument[]) => void;
+};
+
+export const PostAdd = (props: PostAddType) => {
+  const { user, t } = useCore();
+  const [post, setPost] = useState<{
+    loading: boolean;
+    docs: PostDocument[];
+  }>({
+    loading: true,
+    docs: [],
+  });
+  const [state, setState] = useState<{
+    selection: string[];
+  }>({
+    selection: [],
+  });
+
+  const handleChangeSelection = (selection: GridSelectionModel) => {
+    if (Array.isArray(selection)) {
+      setState((s) => ({ ...s, selection: selection as string[] }));
+    }
+  };
+
+  const handleAddPost = () => {
+    props.onAddPost(post.docs.filter(doc => state.selection.includes(doc.id) ));
+    setState((s) => ({ ...s, selection: [] }));
+  };
+
+  useEffect(() => {
+    if (user.loading === false) {
+      if (user.data) {
+        return BookEditCtl.post.many((docs) => {
+          setPost({ loading: false, docs });
+        });
+      } else {
+        setPost({ loading: false, docs: [] });
+      }
+    } else {
+      setPost({ loading: true, docs: [] });
+    }
+    return () => {};
+  }, [user, t]);
+
+  return (
+    <>
+      <Dialog fullWidth maxWidth="sm" open={props.open} onClose={props.onClose}>
+        <DialogTitle>{t("Add Post")}</DialogTitle>
+        <DialogContent>
+          <DataGrid
+            searchable
+            initialState={{
+              pagination: {
+                pageSize: 10,
+              },
+            }}
+            loading={post.loading}
+            rows={post.docs}
+            columns={[
+              {
+                field: "action",
+                headerName: " ",
+                width: 64,
+                renderCell: ({ row }) => <KuiActionIcon tx="restore" />,
+              },
+              {
+                field: "title",
+                headerName: t("Title"),
+                renderCell: ({ row, value }) => value || row.title,
+                width: 280,
+              },
+              {
+                field: "datemodified",
+                headerName: t("Date"),
+                valueGetter: ({ value }) => value?.toMillis?.() || Date.now(),
+                renderCell: ({ value }) => moment(value).format("l LT"),
+                width: 180,
+              },
+            ]}
+            checkboxSelection
+            selectionModel={state.selection}
+            onSelectionModelChange={handleChangeSelection}
+            disableSelectionOnClick={false}
+          />
+        </DialogContent>
+        <DialogActions>
+        <KuiButton
+            tx="confirm"
+            disabled={state.selection.length === 0}
+            onClick={handleAddPost}
+          />
+          <KuiButton tx="close" onClick={props.onClose} />
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
