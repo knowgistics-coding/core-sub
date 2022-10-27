e3418bce0f1feb466fa51c70d0d282b2aaf25c0c
caminkunick <caminkunick@gmail.com>
2022-10-27 12:51:25 +0700

    Controller: updated post, page

--- Controller/post.ts
+++ Controller/post.ts
@@ -1,15 +1,42 @@
 import { User } from "firebase/auth";
-import { collection, onSnapshot, Unsubscribe } from "firebase/firestore";
+import {
+  collection,
+  CollectionReference,
+  doc,
+  DocumentData,
+  DocumentReference,
+  getDoc,
+  onSnapshot,
+  serverTimestamp,
+  Unsubscribe,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../../../controllers/firebase";
-import { PageDoc } from "./page";
+import { VisibilityTabsValue } from "../VisibilityTabs";
+import { PageDate, PageDoc } from "./page";
 
 export class Post extends PageDoc {
   id: string;
 
-  constructor(data?: Partial<Post>) {
+  constructor(
+    data?: Partial<
+      Omit<Post, "datecreate" | "datemodified"> & {
+        datecreate?: PageDate;
+        datemodified?: PageDate;
+      }
+    >
+  ) {
     super(data);
 
     this.id = data?.id ?? "";
+    this.visibility = data?.visibility ?? "private";
+  }
+
+  async save(uid: string): Promise<void> {
+    await updateDoc(Post.doc(uid, this.id), {
+      ...this.toJSON(),
+      datemodified: serverTimestamp(),
+    });
   }
   /**
    * ========================================
@@ -21,15 +48,26 @@ export class Post extends PageDoc {
    *
    * ========================================
    */
-  private static collection(user: User) {
-    return collection(db, "users", user.uid, "docs");
+  private static doc(uid: string, id: string): DocumentReference<DocumentData> {
+    return doc(db, "users", uid, "docs", id);
+  }
+  private static collection(uid: string): CollectionReference<DocumentData> {
+    return collection(db, "users", uid, "docs");
   }
   static watchMy(user: User, callback: (docs: Post[]) => void): Unsubscribe {
-    return onSnapshot(this.collection(user), (snapshot) => {
+    return onSnapshot(this.collection(user.uid), (snapshot) => {
       const docs = snapshot.docs.map(
         (doc) => new Post({ ...doc.data(), id: doc.id })
       );
       callback(docs);
     });
+  }
+  static async getOne(uid: string, id: string): Promise<Post> {
+    const snapshot = await getDoc(this.doc(uid, id));
+    return new Post({ ...snapshot.data(), id });
+  }
+
+  static filter(docs: Post[], visibility: VisibilityTabsValue): Post[] {
+    return docs.filter((doc) => doc.visibility === visibility);
   }
 }
