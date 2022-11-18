6045e7a35010d610ef972bf565040ecd04e222e6
caminkunick <caminkunick@gmail.com>
2022-11-18 15:56:05 +0700

    Controller->Course: change collection to not private

--- Controller/course.ts
+++ Controller/course.ts
@@ -258,7 +258,7 @@ export class Section {
   }
 
   //ANCHOR - collection
-  private static collection(): CollectionReference<DocumentData> {
+  static collection(): CollectionReference<DocumentData> {
     return collection(db, "lms", `${this.prefix}`, "sections");
   }
 
